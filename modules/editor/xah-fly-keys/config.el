a86aea6756fbc747217e632d849b82a0b25059c8
xuyizhe <barrenbass@gmail.com>
2020-11-09 01:11:37 +0800

    Basic configuration

--- modules/editor/xah-fly-keys/config.el
+++
@@ -1,8 +0,0 @@
-;;; xah-fly-keys/config.el -*- lexical-binding: t; -*-
-
-(use-package! xah-fly-keys
-  :init
-  (setq xah-fly-use-control-key nil)
-  :config
-  (xah-fly-keys-set-layout "qwerty")
-  (xah-fly-keys 1))
--- modules/editor/xah-fly-keys/packages.el
+++
@@ -1,4 +0,0 @@
-;; -*- no-byte-compile: t; -*-
-;;; xah-fly-keys/packages.el
-
-(package! xah-fly-keys)
