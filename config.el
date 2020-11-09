bb9aa6d2c5ad91bed91c21ea2bd3619ab9721794
xuyizhe <barrenbass@gmail.com>
2020-11-09 21:09:01 +0800

    Add some key bindings

--- config.el
+++ config.el
@@ -11,15 +11,17 @@
 
 (setq display-line-numbers-type t)
 
-(after! dap-mode
-  (setq dap-cpptools-debug-program `("~/.vscode/extensions/ms-vscode.cpptools-1.1.0/debugAdapters/OpenDebugAD7"))
-  (require 'dap-cpptools))
-
 (use-package! xah-fly-keys
   :init
   (setq xah-fly-use-control-key nil)
   :config
   (xah-fly-keys-set-layout "qwerty")
+  (define-key xah-fly-command-map (kbd "C-. y y") #'youdao-dictionary-search-at-point-posframe)
+  (define-key xah-fly-command-map (kbd "C-. y v") #'youdao-dictionary-play-voice-at-point)
+  (define-key xah-fly-command-map (kbd "C-. y i") #'youdao-dictionary-search-from-input)
+  (define-key xah-fly-command-map (kbd "C-. d d") #'dap-debug)
+  (define-key xah-fly-command-map (kbd "C-. d s") #'dap-ui-show-many-windows)
+  (define-key xah-fly-command-map (kbd "C-. d t") #'dap-breakpoint-toggle)
   (xah-fly-keys 1))
 
 (use-package! prettier-js
@@ -31,8 +33,10 @@
           "--single-quote" "true"
           "--arrow-parens" "always")))
 
+(after! dap-mode
+  (setq dap-cpptools-debug-program `("~/.vscode/extensions/ms-vscode.cpptools-1.1.0/debugAdapters/OpenDebugAD7"))
+  (require 'dap-cpptools))
+
 (after! youdao-dictionary
   (setq url-automatic-caching t)
-  (setq youdao-dictionary-search-history-file "~/.emacs.d/.youdao")
-  (global-set-key (kbd "C-c y y") 'youdao-dictionary-search-at-point-posframe)
-  (global-set-key (kbd "C-c y v") 'youdao-dictionary-play-voice-at-point))
+  (setq youdao-dictionary-search-history-file "~/.emacs.d/.youdao"))
