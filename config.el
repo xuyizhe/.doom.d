a3bb7876737bc27517b71dc6a8bf1050d09e1304
xuyizhe <barrenbass@gmail.com>
2020-12-27 23:27:31 +0800

    Add some urls to lookup-provider-url-alist

--- config.el
+++ config.el
@@ -29,9 +29,11 @@
   (ispell-change-dictionary "en_US" t))
 
 (when (featurep! :tools lookup)
+  (add-to-list '+lookup-provider-url-alist '("Actix-web issues" "https://github.com/actix/actix-web/issues?q=is%%3Aissue+%s"))
+  (add-to-list '+lookup-provider-url-alist '("Docker" "https://docs.docker.com/search/?q=%s"))
   (add-to-list '+lookup-provider-url-alist '("Google" "https://www.google.com/search?ie=utf-8&oe=utf-8&q=%s"))
   (add-to-list '+lookup-provider-url-alist '("Reddit" "https://www.reddit.com/search/?q=%s"))
-  (add-to-list '+lookup-provider-url-alist '("Crates.io" "https://crates.io/search?q=%s"))
+  (add-to-list '+lookup-provider-url-alist '("Crates" "https://crates.io/search?q=%s"))
   (add-to-list '+lookup-provider-url-alist '("MDN" "https://developer.mozilla.org/en-US/search?q=%s"))
   (add-to-list '+lookup-provider-url-alist '("NPM" "https://www.npmjs.com/search?q=%s"))
   (add-to-list '+lookup-provider-url-alist '("Zhihu" "https://www.zhihu.com/search?type=content&q=%s")))
